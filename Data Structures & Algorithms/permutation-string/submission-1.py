96b0c247c8d8dce5ab4f657d7f413f985b108829
Vansh Bhardwaj <141665385+vb10i@users.noreply.github.com>
2026-08-02 17:19:39 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        #just check in 'WINDOW' i.e. len of s1, if all are mapped to 0 then return True else False or keep seaeching
+        if len(s1) > len(s2):
+            return False
+        mapp = {}
+        l = 0
+        lenstr = 0
+        for s in s1:
+            mapp[s] = mapp.get(s, 0)+1
+        
+        for r in range(len(s2)):
+            if s2[r] in mapp:
+                mapp[s2[r]] -= 1
+            if r-l+1 > len(s1):
+                if s2[l] in mapp:
+                    mapp[s2[l]] += 1
+                l += 1
+            if r-l+1 == len(s1):
+                for v in mapp.values():
+                    if v != 0:
+                        break
+                else:
+                    return True 
+        return False 
+
